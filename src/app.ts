bcb125af6b257eacafc4e50f9526540615dd4810
ThundaHorse <abeeeeekim@gmail.com>
2019-09-25 15:51:49 -0400

    refactored to class which implements interface, made v1 & v2 methods

--- src/app.ts
+++ src/app.ts
@@ -1,70 +1,76 @@
-import express, { Application, Request, Response } from "express";
+import express, { Application, Request, Response, response } from "express";
 import bodyParser from "body-parser";
 const path = require("path");
 const app: Application = express();
 app.use(bodyParser.json());
 
-interface RequestData {
+interface StringToParse {
   data: string;
-  getParsed: (req: Request, res: Response) => object;
+  parseEndpoint: (req: Request, res: Response) => object;
 }
 
-var parseData: RequestData = {
-  data: "JOHN0000MICHAEL0009994567",
-  getParsed: function(req: Request, res: Response) {
+class ParseString implements StringToParse {
+  parseEndpoint: (req: express.Request, res: express.Response) => object;
+  constructor(public data: string) {
+    this.data = data;
+  }
+  v1Parse() {
     var output: object,
-      input: string = this.data,
-      letters: any = input.match(/[A-Z]+/gm),
-      zeros: any = input.match(/[\b0]+/gm),
-      id: any = input.match(/[^A-Z0]+/gm);
-    if (req.path === "/api/v1/parse") {
-      output = {
-        statusCode: res.statusCode,
+      letters: any = this.data.match(/[A-Z]+/gm),
+      zeros: any = this.data.match(/[\b0]+/gm),
+      output: object = {
+        statusCode: response.statusCode,
         data: {
           firstName: `${letters[0] + zeros[0]}`,
           lastName: `${letters[1] + zeros[1]}`,
-          clientId: input.match(/[^A-Z0]+/gm)[0]
+          clientId: this.data.match(/[^A-Z0]+/gm)[0]
         }
       };
-    } else {
-      output = {
-        statusCode: res.statusCode,
-        data: {
-          firstName: letters[0],
-          lastName: letters[1],
-          clientId: id[0].slice(0, 3) + "-" + id[0].slice(3)
-        }
-      };
-    }
     return output;
   }
-};
+  v2Parse() {
+    var output: object,
+      letters: any = this.data.match(/[A-Z]+/gm),
+      id: any = this.data.match(/[^A-Z0]+/gm);
+    output = {
+      statusCode: response.statusCode,
+      data: {
+        firstName: letters[0],
+        lastName: letters[1],
+        clientId: id[0].slice(0, 3) + "-" + id[0].slice(3)
+      }
+    };
+    return output;
+  }
+}
 
+var testString: string = "JOHN0000MICHAEL0009994567";
 app
   .route("/api/v1/parse")
-  .get(function(req: Request, res: Response) {
+  .get((req: Request, res: Response) => {
     res.sendFile(path.join(__dirname + "/index.html"));
   })
-  .post(function(req: Request, res: Response) {
+  .post((req: Request, res: Response) => {
     try {
-      res.json(parseData.getParsed(req, res));
-    } catch (err) {
-      console.log(err.message);
-    }
-  });
-app
-  .route("/api/v2/parse")
-  .get(function(req: Request, res: Response) {
-    res.sendFile(path.join(__dirname + "/index.html"));
-  })
-  .post(function(req: Request, res: Response) {
-    try {
-      res.json(parseData.getParsed(req, res));
+      var newRequest = new ParseString(testString);
+      res.json(newRequest.v1Parse());
     } catch (err) {
       console.log(err.message);
     }
   });
 
-app.listen(8080, () => {
-  console.log("Running on port 8080");
-});
+app
+  .route("/api/v2/parse")
+  .get((req: Request, res: Response) => {
+    res.sendFile(path.join(__dirname + "/index.html"));
+  })
+  .post((req: Request, res: Response) => {
+    try {
+      var newRequest = new ParseString(testString);
+      res.json(newRequest.v2Parse());
+    } catch (err) {
+      console.log(err.message);
+    }
+  });
+
+app.listen(8080);
